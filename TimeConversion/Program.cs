38d8a37cc30b6f52e240f5f7dd6a9e95a6b9a6cb
nohaalex <alexnoha37@gmail.com>
2023-06-27 11:18:24 +0530

    Add project files.

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    public class TimeConversion
+    {
+        public static void Main(string[] args)
+        {
+
+            //DateTime time = DateTime.Parse(s);
+            //return time.ToString("HH:mm:ss"); 
+
+            Console.WriteLine("Enter the date string\n");
+
+            string? str = Console.ReadLine();
+
+            ConverTime(str);
+
+        }
+            public static void ConverTime(string str)
+            {
+                DateTime time = DateTime.Parse(str);
+                Console.WriteLine(time.ToString("HH:mm:ss"));
+            }
+
+       
+    }
+}
